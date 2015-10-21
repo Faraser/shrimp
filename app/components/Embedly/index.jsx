bf13637cc0a9d9b0abbc5e84406ca1cfeb431952
Faraser <faraser@rambler.ru>
2015-10-22 02:51:49 +0300

    Fixed some styles.

--- app/components/Embedly/index.jsx
+++ app/components/Embedly/index.jsx
@@ -19,6 +19,7 @@ export default class Embedly extends React.Component {
   }
 
   componentDidMount = () => {
+    console.log('mount');
     if (!this.state.load) {
       fetch(`http://api.embed.ly/1/oembed?url=${this.props.children}&key=${M.API_KEY}`)
         .then(response => response.json())
@@ -27,34 +28,33 @@ export default class Embedly extends React.Component {
           this.setState({data: data, load: true});
         });
     }
-  }
+  };
 
   componentDidUpdate = () => {
     console.log('update');
-  }
+  };
 
   shouldComponentUpdate = (nextProps, nextState) => {
-    if (nextState.load === true && this.state.load === false) {
-      return true;
-    }
-    return false;
-  }
+    return !!(nextState.load && !this.state.load);
+  };
 
   render = () => {
     if (!this.state.load) {
       return (
-        <a href={this.props.children}>{this.props.children}</a>
+        <a
+          href={this.props.children}
+          target='_blank'
+          className='embedly__link'
+        >{this.props.children}</a>
       );
     }
     const data = this.state.data;
     return (
       <div className={cx('embedly', {'embedly_other': this.props.other})}>
         <h3 className='embedly__title'>{data.title}</h3>
-        <a href={data.provider_url} className='embedly__link'>{data.provider_url}</a><br/>
+        <a href={data.provider_url} className='embedly__link' target='_blank'>{data.provider_url}</a><br/>
         <img src={data.thumbnail_url} className='embedly__image'/>
-
         <div className='embedly__descr'>{data.description}</div>
-        <div style={{clear: 'both'}}></div>
       </div>
     );
   };
